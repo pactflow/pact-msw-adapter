4c2f3a2880e98f9a070ffc6f41fa0519b274dba2
YOU54F <yousafn@gmail.com>
2021-05-02 13:57:53 +0100

    generate pact from msw response

--- consumer/src/setupMswPact.js
+++ consumer/src/setupMswPact.js
@@ -1,8 +1,10 @@
 import { rest, response, context } from "msw";
 import { setupServer } from "msw/node";
 const pactData = require("../pact/frontendwebsite-productservice.json");
-
+import { writeFileSync, existsSync, mkdirSync } from "fs";
+var path = require("path");
 const isDebug = process.env.MSW_PACT_DEBUG;
+const writePact = process.env.WRITE_PACT;
 
 function contractToHandlers(contract) {
   return contract.interactions.map((interaction) => {
@@ -10,42 +12,41 @@ function contractToHandlers(contract) {
     const { consumer, provider } = contract;
     isDebug
       ? console.log(
-          `creating msw request mock from pact for consumer: "${consumer.name}" provider: "${provider.name}" method: "${method}" path: "${path}" `
+          `creating msw request mock from pact for consumer: "${
+            consumer.name
+          }" provider: "${provider.name}" request: "${JSON.stringify(
+            interaction.request
+          )}" response: "${JSON.stringify(interaction.response)}" `
         )
       : null;
     return rest[method.toLowerCase()]("http://localhost:8081" + path, () =>
-      createResponse(interaction.response)
+      createResponse(interaction)
     );
   });
 }
 
-function createResponse(expectedResponse) {
-  const { status, headers, body } = expectedResponse;
-  isDebug
-    ? console.log(
-        `setting response to status: "${status}" headers: "${JSON.stringify(
-          headers
-        )}" body: "${JSON.stringify(body)}" `
-      )
-    : null;
+function createResponse(interaction) {
+  const { method, path } = interaction.request;
+  const { status, headers, body } = interaction.response;
+
+  writePact ? writePactToFile(interaction) : null;
   const transformers = [
     context.status(status),
     headers && context.set(headers),
     body && context.body(JSON.stringify(body)),
   ].filter(Boolean);
+
   return response(...transformers);
 }
 const server = setupServer(
-  ...[
-    ...contractToHandlers(pactData),
-    rest.get("*", (req, res, ctx) => {
-      console.log(`No pact interaction defined for ${req.url}`);
-      return res(
-        ctx.status(200),
-        ctx.body(JSON.stringify({ error: "No pact interaction defined" }))
-      );
-    }),
-  ]
+  ...contractToHandlers(pactData),
+  rest.get("*", (req, res, ctx) => {
+    console.log(`No pact interaction defined for ${req.url}`);
+    return res(
+      ctx.status(200),
+      ctx.body(JSON.stringify({ error: "No pact interaction defined" }))
+    );
+  })
 );
 
 beforeAll(() => server.listen());
@@ -53,3 +54,49 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 export { server, rest };
+
+const writePactToFile = (interaction) => {
+  const createPact = {
+    consumer: {
+      name: "interaction.consumer.name",
+    },
+    provider: {
+      name: "interaction.provider.name",
+    },
+    interactions: [
+      {
+        description: interaction.description,
+        providerState: interaction.providerState,
+        request: {
+          method: interaction.request.method,
+          path: interaction.request.path,
+          headers: interaction.request.headers,
+          body: interaction.request.body,
+        },
+        response: {
+          status: interaction.response.status,
+          headers: interaction.response.headers,
+          body: interaction.response.body,
+        },
+      },
+    ],
+    metadata: {
+      pactSpecification: {
+        version: "2.0.0",
+      },
+    },
+  };
+
+  var filePath = `./msw_generated_pacts/msw_pact_${interaction.description}.json`;
+  ensureDirExists(filePath);
+  writeFileSync(filePath, JSON.stringify(createPact));
+};
+
+const ensureDirExists = (filePath) => {
+  var dirname = path.dirname(filePath);
+  if (existsSync(dirname)) {
+    return true;
+  }
+  ensureDirExists(dirname);
+  mkdirSync(dirname);
+};
