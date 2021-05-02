c594b39106e61477442a33511c58dd61ec7c8569
YOU54F <yousafn@gmail.com>
2021-05-02 19:16:27 +0100

    intercept msw mock matched request/response

--- consumer/src/pactFromMsw.msw.spec.js
+++ consumer/src/pactFromMsw.msw.spec.js
@@ -1,18 +1,31 @@
 import API from "./api";
-import { rest, response, context } from "msw";
+import { rest } from "msw";
 import { setupServer } from "msw/node";
 
-const server = setupServer(
-  rest.get("*", (req, res, ctx) => {
-    console.log(`No pact interaction defined for ${req.url}`);
-    return res(
-      ctx.status(200),
-      ctx.body(JSON.stringify({ error: "No pact interaction defined" }))
-    );
-  })
-);
+const server = setupServer();
+
 describe("API - With MSW mock generating a pact", () => {
-  beforeAll(() => server.listen());
+  beforeAll(() => {
+    server.listen();
+    var requestMatch = new Promise((resolve) => {
+      server.on("request:match", resolve);
+    });
+    var responseMocked = new Promise((resolve) => {
+      server.on("response:mocked", resolve);
+    });
+
+    Promise.all([requestMatch, responseMocked]).then((data) => {
+      console.log("Request matched and response mocked");
+      const request = data[0]; // MockedRequest<DefaultRequestBody>
+      const response = data[1]; // IsomorphicResponse;
+      console.log(request);
+      console.log(response);
+    });
+    server.on("request:unhandled", (unhandled) => {
+      const { url } = unhandled;
+      console.log("This request was unhandled by msw: " + url);
+    });
+  });
   afterEach(() => server.resetHandlers());
   afterAll(() => server.close());
   test("get all products", async () => {
@@ -47,5 +60,11 @@ describe("API - With MSW mock generating a pact", () => {
 
     const respProduct = await API.getProduct("10");
     expect(respProduct).toEqual(product);
+  });
+
+  test("unhandled route", async () => {
+    await expect(API.getProduct("10")).rejects.toThrow(
+      "connect ECONNREFUSED 127.0.0.1:8081"
+    );
   });
 });
