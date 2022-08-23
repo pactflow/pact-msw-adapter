1c8aa9a35898be4280c99859d795b29a219b3bc6
Yousaf Nabi <yousaf@pactflow.io>
2022-08-23 17:17:20 +0100

    feat: Add client name/version into pact file

--- src/pactFromMswServer.msw.spec.ts
+++ src/pactFromMswServer.msw.spec.ts
@@ -2,6 +2,7 @@ import API from "../examples/react/src/api";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 import { PactFile, setupPactMswAdapter } from "./pactMswAdapter";
+const pjson = require("../package.json");
 
 const server = setupServer();
 const pactMswAdapter = setupPactMswAdapter({
@@ -9,13 +10,13 @@ const pactMswAdapter = setupPactMswAdapter({
   options: {
     consumer: "testConsumer",
     providers: {
-      ['testProvider']: ['products'],
-      ['testProvider2']: ['/product/10'],
+      ["testProvider"]: ["products"],
+      ["testProvider2"]: ["/product/10"],
     },
     debug: true,
-    includeUrl: ['products', '/product'],
-    excludeUrl: ['/product/11'],
-    excludeHeaders: ["x-powered-by","cookie"]
+    includeUrl: ["products", "/product"],
+    excludeUrl: ["/product/11"],
+    excludeHeaders: ["x-powered-by", "cookie"],
   },
 });
 
@@ -59,14 +60,14 @@ describe("API - With MSW mock generating a pact", () => {
 
   test("post product ID 10", async () => {
     const productData = {
-      "type": "CREDIT_CARD",
-      "name": "28 Degrees",
+      type: "CREDIT_CARD",
+      name: "28 Degrees",
     };
-     server.use(
-       rest.post(API.url + "/product/10", (req, res, ctx) => {
-         return res(ctx.status(200), ctx.json(productData));
-       })
-     );
+    server.use(
+      rest.post(API.url + "/product/10", (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(productData));
+      })
+    );
 
     const respProduct = await API.postProduct("10", productData);
     expect(respProduct).toEqual(productData);
@@ -92,22 +93,23 @@ describe("API - With MSW mock generating a pact", () => {
     const product = {
       id: "10",
       type: "CREDIT_CARD",
-      name: "28 Degrees"
+      name: "28 Degrees",
     };
     const hiddenVisibilityProduct = {
       ...product,
-      visibility: "hidden"
+      visibility: "hidden",
     };
     server.use(
       rest.get(API.url + "/product/10", (req, res, ctx) => {
-        const visibility = req.url.searchParams.get('visibility');
-        const response = visibility === 'hidden' ? hiddenVisibilityProduct : product;
+        const visibility = req.url.searchParams.get("visibility");
+        const response =
+          visibility === "hidden" ? hiddenVisibilityProduct : product;
 
         return res(ctx.status(200), ctx.json(response));
       })
     );
 
-    const respProduct = await API.getProduct("10", {visibility: "hidden"});
+    const respProduct = await API.getProduct("10", { visibility: "hidden" });
     expect(respProduct).toEqual(hiddenVisibilityProduct);
   });
 
@@ -118,41 +120,47 @@ describe("API - With MSW mock generating a pact", () => {
   });
 
   test("creates pact files", async () => {
-    let pactResults: PactFile[] = []
-    await pactMswAdapter.writeToFile((path, data) => { pactResults.push(data as PactFile) }); // writes the pacts to a file
-    expect(pactResults.length).toEqual(2)
-    expect(pactResults[0].consumer.name).toEqual('testConsumer')
-    expect(pactResults[0].provider.name).toEqual('testProvider')
-    expect(pactResults[1].consumer.name).toEqual('testConsumer')
-    expect(pactResults[1].provider.name).toEqual('testProvider2')
-    expect(pactResults[0].interactions[0].request.method).toEqual('GET')
-    expect(pactResults[0].interactions[0].request.path).toEqual('/products')
+    let pactResults: PactFile[] = [];
+    await pactMswAdapter.writeToFile((path, data) => {
+      pactResults.push(data as PactFile);
+    }); // writes the pacts to a file
+    expect(pactResults.length).toEqual(2);
+    expect(pactResults[0].consumer.name).toEqual("testConsumer");
+    expect(pactResults[0].provider.name).toEqual("testProvider");
+    expect(pactResults[1].consumer.name).toEqual("testConsumer");
+    expect(pactResults[1].provider.name).toEqual("testProvider2");
+    expect(pactResults[0].interactions[0].request.method).toEqual("GET");
+    expect(pactResults[0].interactions[0].request.path).toEqual("/products");
     expect(pactResults[0].interactions[0].request.headers).toEqual({
-      "accept": "application/json, text/plain, */*",
-      "authorization": expect.any(String) ,
+      accept: "application/json, text/plain, */*",
+      authorization: expect.any(String),
       "user-agent": expect.any(String),
-    })
-    expect(pactResults[0].interactions[0].response.status).toEqual(200)
+    });
+    expect(pactResults[0].interactions[0].response.status).toEqual(200);
 
     expect(pactResults[0].interactions[0].response.headers).toEqual({
-      "content-type": "application/json"
-    })
+      "content-type": "application/json",
+    });
     expect(pactResults[0].interactions[0].response.body).toEqual([
       {
-        "id": "09",
-        "type": "CREDIT_CARD",
-        "name": "Gem Visa"
-      }
-    ])
+        id: "09",
+        type: "CREDIT_CARD",
+        name: "Gem Visa",
+      },
+    ]);
     expect(pactResults[1].interactions[0].request.body).toEqual({
-      "type": "CREDIT_CARD",
-      "name": "28 Degrees"
-    })
-    expect(pactResults[1].interactions[1].request.body).toBeUndefined()
+      type: "CREDIT_CARD",
+      name: "28 Degrees",
+    });
+    expect(pactResults[1].interactions[1].request.body).toBeUndefined();
     expect(pactResults[0].metadata).toEqual({
-      "pactSpecification": {
-        "version": "2.0.0"
-      }
-    })
-  })
+      pactSpecification: {
+        version: "2.0.0",
+      },
+      client: {
+        name: "pact-msw-adapter",
+        version: pjson.version,
+      },
+    });
+  });
 });
